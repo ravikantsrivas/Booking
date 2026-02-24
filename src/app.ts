6f7f54c6f7978113a2b9246af72c2dd340d1e23f
Ravikant Srivas <ravikantsrivas0046@gmail.com>
2026-02-24 11:23:53 +0530

    forth commit

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Booking API");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/equipments", equipmentRoutes);
