9670fce16dbe4825ff2aad5e7dfb406ef44903a9
Ravikant Srivas <ravikantsrivas0046@gmail.com>
2026-02-27 18:11:15 +0530

    new

--- src/lib/mailtrap.ts
+++ src/lib/mailtrap.ts
@@ -5,11 +5,14 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, 
       },
-      secure: false, // true for 465, false for other ports
+     tls: {
+       rejectUnauthorized: false // helps with some hosting environments
+     }
     });
 
     const mailOptions = {
